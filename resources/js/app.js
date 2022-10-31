f5a2629f45c996548aa900060c97e3101ba006fe
sakamotosora0116 <soras.k.m.tj16@gmail.com>
2022-10-30 20:06:14 -0700

    router_ga_dekiruyouninatta

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,30 @@
 
-import {createApp} from 'vue'
-// import e
+// import {createApp} from 'vue'
+import { createApp } from 'vue/dist/vue.esm-bundler';
 
-import App from './App.vue'
+// import App from './App.vue'
 
-createApp(App).mount("#app")
+import * as VueRouter from 'vue-router'
+
+import ExampleComponent from './components/ExampleComponent.vue'
+
+import AboutComponent from './components/AboutComponent.vue'
+
+const routes = [
+    {path: '/', component: ExampleComponent},
+    {path: '/about', component: AboutComponent},
+]
+
+const router = VueRouter.createRouter({
+    history: VueRouter.createWebHistory(),
+    routes,
+})
+
+const app = createApp({})
+
+app.use(router)
+
+app.component('example-component', ExampleComponent)
+// app.component('about-component', AboutComponent)
+
+app.mount('#app')
---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>Hello</h1>
+    <h2>{{ title }}</h2>
+</template>
+
+<!-- <script>
+export default {
+    setup: () => ({
+        title: 'how to install iuouuoiuo'
+    })
+}
+</script> -->
+
+<script>
+export default {
+    setup() {
+        const title = 'Welcome to the example page'
+        return {
+            title
+        }
+    }
+
+}
+</script>
